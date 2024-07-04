719e71f1d8e638938c9738070655a42c74540933
Vickygreene123 <okereaforfavour867@gmail.com>
2024-07-04 09:55:04 +0000

    changed icon

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -11,7 +11,7 @@ const HomePage = () => {
       <Hero />
       <TrustedSchools />
       <Products />
-      {/* <Testimonials /> */}
+      <Testimonials />
       <Accordion />
     </div>
   );
