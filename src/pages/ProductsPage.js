fdb8da23745a0a510ea0e2796a963d847cad61b8
Vickygreene123 <okereaforfavour867@gmail.com>
2024-07-16 01:16:52 +0000

    rrrrr

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -172,7 +172,7 @@ const ProductsPage = () => {
             PRODUCTS
           </p>
 
-          <div className='md:max-w-[78%] lg:max-w-[85%] xl:max-w-[60%]'>
+          <div className='md:max-w-[78%] lg:max-w-[100%] xl:max-w-[60%]'>
             <h1 className='text-3xl text-[#081640] font-bold tracking-tighter sm:text-4xl sm:leading-snug lg:text-5xl lg:leading-[72px] xl:text-[3.5rem]'>
               Shaping Africa's <span className='text-[#f18337]'>med excellence</span> 
               </h1> <h1 className="text-3xl text-[#081640] font-bold tracking-tighter sm:text-4xl sm:leading-snug lg:text-5xl lg:leading-[72px] xl:text-[3.5rem]" > with our{" "}
